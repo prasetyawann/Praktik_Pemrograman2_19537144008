b9f195d85791a6d173697ba2f5d5a6005ab27569
prasetyawann <prasetyawancaesar.2019@uny.ac.id>
2020-06-10 20:26:37 +0700

    menambahkan projectnya pak

--- project liquid/LiquidCRUD/src/Liquid.java
+++ project liquid/LiquidCRUD/src/Liquid.java
@@ -333,6 +333,9 @@ private void reset(){
             java.util.logging.Logger.getLogger(Liquid.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
